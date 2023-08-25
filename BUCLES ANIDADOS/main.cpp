9cd6a8b91025941d8a3b9b7883354ea8b7f7d1d9
RodATS <77297145+RodATS@users.noreply.github.com>
2023-08-24 23:40:49 -0500

    Update main.cpp

--- BUCLES ANIDADOS/main.cpp
+++ BUCLES ANIDADOS/main.cpp
@@ -1,7 +1,8 @@
+
 #include <chrono>
 #include <iostream>
 
-#define MAX 10
+#define MAX 10000
 using namespace std;
 
 double A[MAX][MAX], x[MAX], y[MAX];
@@ -9,7 +10,18 @@ double A[MAX][MAX], x[MAX], y[MAX];
 
 int main() {
   int i, j;
+  
+  for(int aux = 0; aux < MAX; aux++)
+    {
+      x[aux] = aux;
+      y[aux] = aux;
+    }
 
+  for(i = 0; i < MAX; i++)
+    for (j = 0; j < MAX; j++)
+      A[i][j] = 1;
+
+  
   const auto start = std::chrono::steady_clock::now();
   
 
@@ -21,7 +33,7 @@ int main() {
 
   const auto end = std::chrono::steady_clock::now();
   const std::chrono::duration<double> timepo = end - start;
-  cout << timepo.count() <<endl;
+  cout<< "Prueba Bucle 1: " << timepo.count() <<endl;
 
   
   const auto start2 = std::chrono::steady_clock::now();
@@ -33,6 +45,6 @@ int main() {
   const auto end2 = std::chrono::steady_clock::now();
   const std::chrono::duration<double> elapsed_seconds2 = end2 - start2;
 
-  cout << elapsed_seconds2.count() <<endl;
+  cout << "Prueba Bucle 2: "<<elapsed_seconds2.count() <<endl;
 
 }
