7a0eae0cb99b6ebaa210e99ba838fb6e2a6a15f1
RodATS <77297145+RodATS@users.noreply.github.com>
2023-09-21 02:52:01 -0500

    Create test.cpp

---
+++ ConfiguracionMPI-cluster/test.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+int myrank, nprocs;
+
+MPI_Init(&argc, &argv);
+MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+printf("Hello world from processor %s, rank %d out of %d processors\n", processor_name, world_rank, world_size);
+MPI_Finalize();
+return 0;
+}
